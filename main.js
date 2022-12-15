07786cda7c62f098ca386c6949b71509ed025166
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-15 08:54:36 -0800

    index

---
+++ main.js
@@ -0,0 +1,18 @@
+let a=(prompt("Enter the value"));
+
+function subMark() {
+        for(let i=1; i<=3; i++){
+
+                document.write(a + "<br>")
+                
+            }
+    
+    }
+          
+    
+    subMark(a);
+
+    
+    
+    
+    
